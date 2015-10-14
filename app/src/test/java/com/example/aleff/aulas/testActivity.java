66a582170bbc765434ca6d74e2f0c12ad6c9f393
Aleff <aleff.matos@mobwise.com.br>
2015-10-14 19:07:24 -0300

    blab7

---
+++ app/src/test/java/com/example/aleff/aulas/testActivity.java
@@ -0,0 +1,16 @@
+package com.example.aleff.aulas;
+
+import android.widget.EditText;
+
+/**
+ * Created by Aleff on 14/10/2015.
+ */
+public class testActivity {
+
+        public void test1 () throws Exception
+        {
+            EditText resposta;
+
+
+        }
+}
